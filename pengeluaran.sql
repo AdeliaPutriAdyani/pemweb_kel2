aa8f683bf703844990af0feb381acb8b13f3d779
21081010047AdeliaPutriAdyani <21081010047@student.upnjatim.ac.id>
2023-05-30 13:47:27 +0700

    database

--- pengeluaran.sql
+++ pengeluaran.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2023 at 08:25 AM
+-- Generation Time: May 30, 2023 at 08:45 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -24,13 +24,57 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `catatan_keuangan`
+--
+
+CREATE TABLE `catatan_keuangan` (
+  `id_catatan` varchar(255) NOT NULL,
+  `id_login` varchar(255) DEFAULT NULL,
+  `id_kategori` varchar(255) DEFAULT NULL,
+  `minggu` date DEFAULT NULL,
+  `bulan` date DEFAULT NULL,
+  `tahun` date DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
+  `total_pemasukan` varchar(255) DEFAULT NULL,
+  `total_pengeluaran` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` varchar(255) NOT NULL,
+  `nama_kategori` varchar(255) DEFAULT NULL,
+  `gambar` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id_login` varchar(255) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `PASSWORD` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pemasukan`
 --
 
 CREATE TABLE `pemasukan` (
-  `id_pemasukan` varchar(255) DEFAULT NULL,
-  `id_user` varchar(255) DEFAULT NULL,
+  `id_pemasukan` varchar(255) NOT NULL,
+  `id_login` varchar(255) DEFAULT NULL,
   `id_rekening` varchar(255) DEFAULT NULL,
+  `id_kategori` varchar(255) DEFAULT NULL,
   `tanggal` date DEFAULT NULL,
   `jumlah_pemasukan` varchar(255) DEFAULT NULL,
   `keterangan` varchar(255) DEFAULT NULL
@@ -43,9 +87,10 @@ CREATE TABLE `pemasukan` (
 --
 
 CREATE TABLE `pengeluaran` (
-  `id_pengeluaran` varchar(255) DEFAULT NULL,
-  `id_user` varchar(255) DEFAULT NULL,
+  `id_pengeluaran` varchar(255) NOT NULL,
+  `id_login` varchar(255) DEFAULT NULL,
   `id_rekening` varchar(255) DEFAULT NULL,
+  `id_kategori` varchar(255) DEFAULT NULL,
   `tanggal` date DEFAULT NULL,
   `jumlah_pengeluaran` varchar(255) DEFAULT NULL,
   `keterangan` varchar(255) DEFAULT NULL
@@ -59,37 +104,10 @@ CREATE TABLE `pengeluaran` (
 
 CREATE TABLE `rekening` (
   `id_rekening` varchar(255) NOT NULL,
+  `id_login` varchar(255) DEFAULT NULL,
   `nama_rekening` varchar(255) DEFAULT NULL,
-  `total_rekening` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `statistik`
---
-
-CREATE TABLE `statistik` (
-  `id_statistik` varchar(255) DEFAULT NULL,
-  `id_user` varchar(255) DEFAULT NULL,
-  `minggu` date DEFAULT NULL,
-  `bulan` date DEFAULT NULL,
-  `tahun` date DEFAULT NULL,
-  `total_pemasukan` varchar(255) DEFAULT NULL,
-  `total_pengeluaran` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id_user` varchar(255) NOT NULL,
-  `nama_user` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `PASSWORD` varchar(255) DEFAULT NULL
+  `saldo_rekening` varchar(255) DEFAULT NULL,
+  `total` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
@@ -97,60 +115,82 @@ CREATE TABLE `user` (
 --
 
 --
+-- Indexes for table `catatan_keuangan`
+--
+ALTER TABLE `catatan_keuangan`
+  ADD PRIMARY KEY (`id_catatan`),
+  ADD KEY `id_login` (`id_login`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
 -- Indexes for table `pemasukan`
 --
 ALTER TABLE `pemasukan`
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_rekening` (`id_rekening`);
+  ADD PRIMARY KEY (`id_pemasukan`),
+  ADD KEY `id_login` (`id_login`),
+  ADD KEY `id_rekening` (`id_rekening`),
+  ADD KEY `id_kategori` (`id_kategori`);
 
 --
 -- Indexes for table `pengeluaran`
 --
 ALTER TABLE `pengeluaran`
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_rekening` (`id_rekening`);
+  ADD PRIMARY KEY (`id_pengeluaran`),
+  ADD KEY `id_login` (`id_login`),
+  ADD KEY `id_rekening` (`id_rekening`),
+  ADD KEY `id_kategori` (`id_kategori`);
 
 --
 -- Indexes for table `rekening`
 --
 ALTER TABLE `rekening`
-  ADD PRIMARY KEY (`id_rekening`);
-
---
--- Indexes for table `statistik`
---
-ALTER TABLE `statistik`
-  ADD KEY `id_user` (`id_user`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`id_rekening`),
+  ADD KEY `id_login` (`id_login`);
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `catatan_keuangan`
+--
+ALTER TABLE `catatan_keuangan`
+  ADD CONSTRAINT `catatan_keuangan_ibfk_1` FOREIGN KEY (`id_login`) REFERENCES `login` (`id_login`),
+  ADD CONSTRAINT `catatan_keuangan_ibfk_2` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`);
+
+--
 -- Constraints for table `pemasukan`
 --
 ALTER TABLE `pemasukan`
-  ADD CONSTRAINT `pemasukan_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
-  ADD CONSTRAINT `pemasukan_ibfk_2` FOREIGN KEY (`id_rekening`) REFERENCES `rekening` (`id_rekening`);
+  ADD CONSTRAINT `pemasukan_ibfk_1` FOREIGN KEY (`id_login`) REFERENCES `login` (`id_login`),
+  ADD CONSTRAINT `pemasukan_ibfk_2` FOREIGN KEY (`id_rekening`) REFERENCES `rekening` (`id_rekening`),
+  ADD CONSTRAINT `pemasukan_ibfk_3` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`);
 
 --
 -- Constraints for table `pengeluaran`
 --
 ALTER TABLE `pengeluaran`
-  ADD CONSTRAINT `pengeluaran_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
-  ADD CONSTRAINT `pengeluaran_ibfk_2` FOREIGN KEY (`id_rekening`) REFERENCES `rekening` (`id_rekening`);
+  ADD CONSTRAINT `pengeluaran_ibfk_1` FOREIGN KEY (`id_login`) REFERENCES `login` (`id_login`),
+  ADD CONSTRAINT `pengeluaran_ibfk_2` FOREIGN KEY (`id_rekening`) REFERENCES `rekening` (`id_rekening`),
+  ADD CONSTRAINT `pengeluaran_ibfk_3` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`);
 
 --
--- Constraints for table `statistik`
+-- Constraints for table `rekening`
 --
-ALTER TABLE `statistik`
-  ADD CONSTRAINT `statistik_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+ALTER TABLE `rekening`
+  ADD CONSTRAINT `rekening_ibfk_1` FOREIGN KEY (`id_login`) REFERENCES `login` (`id_login`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
